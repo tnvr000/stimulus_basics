58e2abcff09f6a88f069aa77780c732d5b2a5944
Tanveer Ahmad Khan <tanveerk@mindfiresolutions.com>
2021-11-25 22:30:50 +0530

    added stimulus interactivity

--- src/controllers/todo_controller.js
+++ src/controllers/todo_controller.js
@@ -1,7 +1,25 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  connect() {
-    console.log('todo connected');
+  static targets = ['tasksContainer', 'taskToAdd']
+  addTask() {
+    if(this.taskToAddTarget.value.length == 0) {
+      alert('Please enter a task');
+    }
+    this.tasksContainerTarget.innerHTML += `
+      <div class="task">
+        <span id="taskname">
+          ${ this.taskToAddTarget.value }
+        </span>
+        <button class="delete" data-action="todo#deleteTask">
+          Delete
+        </button>
+      </div>
+    `;
+    this.taskToAddTarget.value = '';
+  }
+
+  deleteTask(event) {
+    event.target.parentNode.remove();
   }
 }
